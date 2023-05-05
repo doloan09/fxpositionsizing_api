f9a44eb7b249183b0d88d9f7646e86304f6c19c0
Do Thi Loan <dothiloan150901@gmail.com>
2023-05-05 17:18:30 +0700

    schedule - command currency

---
+++ app/Console/Commands/CurrencyExcRateCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\CurrencyExcRate;
+use Illuminate\Console\Command;
+
+class CurrencyExcRateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'currency';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = env('URL_ALPHA_VANTAGE');
+        $apikey = env('API_KEY');
+        $arr_currency_main = ['USD', 'EUR', 'GBP', 'AUD', 'CAD', 'CHF']; //'USDJPY', 'EURJPY', 'GBPJPY', 'AUDJPY', 'CADJPY', 'CHFJPY'
+        $arr_currency = ['EUR', 'GBP', 'AUD', 'CAD']; //'EURUSD', 'GBPUSD', 'AUDUSD', 'CADUSD'
+
+        foreach ($arr_currency_main as $item){
+            $url .= '&from_currency=' . $item . '&to_currency=JPY&apikey=' . $apikey;
+            $json = file_get_contents($url);
+            $data = json_decode($json,true);
+
+            if ($data){
+                $data_currency['from_currency_code'] = $data["Realtime Currency Exchange Rate"]["1. From_Currency Code"];
+                $data_currency['from_currency_name'] = $data["Realtime Currency Exchange Rate"]["2. From_Currency Name"];
+                $data_currency['to_currency_code']   = $data["Realtime Currency Exchange Rate"]["3. To_Currency Code"];
+                $data_currency['to_currency_name']   = $data["Realtime Currency Exchange Rate"]["4. To_Currency Name"];
+                $data_currency['exchange_rate']      = $data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+                $data_currency['last_refreshed']     = $data["Realtime Currency Exchange Rate"]["6. Last Refreshed"];
+                $data_currency['time_zone']          = $data["Realtime Currency Exchange Rate"]["7. Time Zone"];
+                $data_currency['bid_price']          = $data["Realtime Currency Exchange Rate"]["8. Bid Price"];
+                $data_currency['ask_price']          = $data["Realtime Currency Exchange Rate"]["9. Ask Price"];
+
+                CurrencyExcRate::query()->updateOrCreate([
+                    'from_currency_code' => $data_currency['from_currency_code'],
+                    'to_currency_code' => $data_currency['to_currency_code'],
+                ], $data_currency);
+
+                if ($item === 'USD'){
+                    foreach ($arr_currency as $i){
+                        $data_currency['from_currency_code'] = $i;
+                        $data_currency['from_currency_name'] = $i;
+                        $data_currency['to_currency_code'] = 'USD';
+                        $data_currency['to_currency_name'] = 'United States Dollar';
+
+                        CurrencyExcRate::query()->updateOrCreate([
+                            'from_currency_code' => $data_currency['from_currency_code'],
+                            'to_currency_code' => $data_currency['to_currency_code'],
+                        ], $data_currency);
+                    }
+                }elseif ($item === 'CHF'){  //USDCHF
+                    $data_currency['from_currency_code'] = 'USD';
+                    $data_currency['from_currency_name'] = 'United States Dollar';
+                    $data_currency['to_currency_code'] = 'CHF';
+                    $data_currency['to_currency_name'] = 'Swiss Franc';
+
+                    CurrencyExcRate::query()->updateOrCreate([
+                        'from_currency_code' => $data_currency['from_currency_code'],
+                        'to_currency_code' => $data_currency['to_currency_code'],
+                    ], $data_currency);
+                }
+            }
+
+            sleep(30);
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \Illuminate\Console\KeyGenerateCommand::class,
+        \App\Console\Commands\CurrencyExcRateCommand::class,
     ];
 
     /**
@@ -24,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('currency')->everyThirtyMinutes();
     }
 }
